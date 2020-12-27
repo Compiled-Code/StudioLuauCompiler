dd4ad302447a26c63afe78bb5d5b8e6f8eb131bf
Compiled-Code <75095310+Compiled-Code@users.noreply.github.com>
2020-12-27 04:15:40 +0000

    Update core.cpp

--- core.cpp
+++ core.cpp
@@ -15,18 +15,26 @@ inline void alloc_console()
 	SetConsoleTitleA("Luau Studio Compiler | CompiledCode#1234");
 }
 
-struct luau_compiler_option
+struct luau_compiler_option_1
 {
 	std::uint32_t v1, v2, v3, v4;
 };
 
-using luau_compiler_t = std::string(__fastcall*)(const std::string&, const luau_compiler_option&, const std::uint16_t&, std::uint64_t);
+struct luau_compiler_option_2
+{
+	std::uint8_t v1, v2;
+};
+
+using luau_compiler_t = std::string(__fastcall*)(const std::string&, const luau_compiler_option_1&, const luau_compiler_option_2&);
 const auto luau_compile = reinterpret_cast<luau_compiler_t>(reinterpret_cast<std::uintptr_t>(GetModuleHandleA(nullptr)) + 0x179EF00);
 
 void core()
 {
 	alloc_console();
-	const auto bytecode = luau_compile("warn'a'", { 1, 1, 2, 0 }, 257, 0);
+	const auto bytecode = luau_compile("do end", { 1, 1, 2, 0 }, { 0, 0 });
+
+	for (const auto& b : bytecode)
+		printf("%02X ", b);
 }
 
 bool __stdcall DllMain(const HMODULE dllHandle, const DWORD reason, const void*)
